c1087c5184eb12ff6ccb5eea263fe089076a665d
Tobiloba Orekoya <tobilobaorekoya19@gmail.com>
2023-02-15 04:05:17 +0100

    initial commit

---
+++ images/index.js
@@ -0,0 +1,33 @@
+import annual from "./annual.svg";
+import apple from "./apple.svg";
+import coin from "./coin.png";
+import cup from "./cup.png";
+import fliplogo from "./fliplogo.svg";
+import flippacked from "./flippacked.png";
+import goggles from "./goggles.png";
+import googleplay from "./googleplay.svg";
+import heroimage from "./heroimage.png";
+import hourglass from "./hourglass.svg";
+import lockup from "./lockup.svg";
+import react from "./react.svg";
+import star from "./star.png";
+import time from "./time.svg";
+import wallet from "./wallet.png";
+
+export {
+  annual,
+  apple,
+  coin,
+  cup,
+  fliplogo,
+  flippacked,
+  goggles,
+  googleplay,
+  heroimage,
+  hourglass,
+  lockup,
+  react,
+  star,
+  time,
+  wallet,
+};
